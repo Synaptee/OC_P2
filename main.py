44a64405b064a1ec130da2a080d90477ae8acdd4
Synaptee <jba.munoz@gmail.com>
2023-03-31 16:39:48 +0200

    Extraction de toutes les catégories + création de fichiers csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from slugify import slugify
+import csv
 
 session = requests.session()
 
@@ -31,14 +32,17 @@ def scrape_url(url_to_scrape) -> list:
     price_notax = soup.find('th', string='Price (excl. tax)').find_next_sibling('td').get_text()
     price_tax = soup.find('th', string='Price (incl. tax)').find_next_sibling('td').get_text()
     availability = soup.find('th', string='Availability').find_next_sibling('td').get_text()
-    description = soup.find('div', id='product_description').find_next('p').get_text()
+    try:
+        description = soup.find('div', id='product_description').find_next('p').get_text()
+    except:
+        description = "Ce livre n'a pas de description"
     category = soup.select('.breadcrumb > li > a')[-1].get_text()
     rating_init = soup.find('p', class_='star-rating')  # Renvoie tout le code HTML
     rating_class = rating_init['class']  # Je récupère le nom de la classe qui est en fait une liste
     rating = rating_class[1]  # Je récupère le second élément de la liste
     img = 'http://books.toscrape.com/' + soup.select('img')[0]['src']
 
-    nom_img = slugify(category+" "+title, separator="_") + ".jpg"
+    nom_img = slugify(category + " " + title, separator="_") + ".jpg"
     try:
         load_img(img, nom_img)
     except Exception as err:
@@ -49,9 +53,53 @@ def scrape_url(url_to_scrape) -> list:
     return datas
 
 
+def categ_to_scrape(base_url, categ):
+    filecsv = categ + ".csv"
+    with open(filecsv, 'a') as f:
+
+        nb = 0
+
+        # URL de la première page de la catégorie de livres
+        url = base_url + 'index.html'
+
+        basik_url = 'https://books.toscrape.com/catalogue'
+
+        # Boucle pour parcourir toutes les pages de livres
+        while url:
+
+            soup = bs_get(url)
+
+            # Récupération des informations de chaque livre sur la page
+            books = soup.select('article.product_pod')
+            for book in books:
+                url_book = basik_url + book.h3.a['href'][8:]
+                nb += 1
+                datas = scrape_url(url_book)
+                writer = csv.writer(f)
+                writer.writerow(datas)
+
+            # Récupération du lien de la page suivante
+            next_page = soup.select_one('li.next a')
+            if next_page:
+                # Construction de l'URL absolue de la page suivante
+                url = base_url + next_page['href']
+            else:
+                url = None
+
+        print(nb)
+
+
 def main():
-    url_to_scrape = 'https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html'
-    print(scrape_url(url_to_scrape))
+    base_url = 'http://books.toscrape.com/'
+    soup = bs_get(base_url)
+    # Récupération des liens de chaque catégorie sur la page d'accueil
+    category_links = soup.select('ul.nav-list > li > ul > li > a')
+
+    for category_link in category_links:
+        categ = category_link.get_text().strip()
+        url = base_url + category_link['href']
+        # print(url)
+        categ_to_scrape(url[:-10], categ)
 
 
 if __name__ == "__main__":
