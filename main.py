ad1e122f35040abe4e0bd6d88a2d2b4b120995ce
Synaptee <jba.munoz@gmail.com>
2023-04-06 11:35:08 +0200

    Changement de noms de certaines variables et fonctions pour les rendre plus explicites

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def load_img(img, nom_img, categ):
 
 
 def load_csv(csv_path: str, books_datas: list):
-    """Fonction qui reçoit en paramètre le chemin du fichier CSV à créer et la liste des dictionnaires contenant les données à écrire, 
-    et qui écrit ces données dans le fichier CSV"""
+    """Fonction qui reçoit en paramètre le chemin du fichier CSV à créer et la liste des dictionnaires contenant les
+    données à écrire, et qui écrit ces données dans le fichier CSV"""
     champs = ['product_page_url', 'universal_product_code', 'title', 'price_including_tax', 'price_excluding_tax',
               'number_available', 'product_description', 'category', 'review_rating', 'image_url']
     with open(csv_path, mode='w', newline='') as f:
@@ -43,7 +43,7 @@ def convert_rating(txt):
     return ratings.get(txt, 0)
 
 
-def convert_stock(txt):
+def convert_stock(txt: str) -> int:
     """Fonction qui convertit le texte de la quantité en stock en nombre"""
     stock = ""
     for char in txt:
@@ -57,8 +57,9 @@ def convert_stock(txt):
         return 0
 
 
-def scrape_url(url_to_scrape) -> dict:
-    """Fonction qui reçoit une URL en paramètre et qui retourne un dictionnaire contenant l'ensemble des éléments demandés"""
+def scrape_url(url_to_scrape: str) -> dict:
+    """Fonction qui reçoit une URL en paramètre et qui retourne un dictionnaire contenant
+    l'ensemble des éléments demandés"""
     soup = bs_get(url_to_scrape)
     if soup is None:
         exit()
@@ -93,10 +94,10 @@ def scrape_url(url_to_scrape) -> dict:
     return datas
 
 
-def categ_to_scrape(base_url, categ):
+def get_books_from_category(base_url: str, category_name: str):
     """Fonction qui parcourt toutes les pages d'une catégorie et extrait les informations de chaque livre"""
-    print(f'Récupération de la catégorie {categ} en cours. Veuillez patitenter...')
-    csv_path = os.path.join(slugify(categ, separator="_"), f'{categ}.csv')
+    print(f'Récupération de la catégorie {category_name} en cours. Veuillez patitenter...')
+    csv_path = os.path.join(slugify(category_name, separator="_"), f'{category_name}.csv')
     books_datas = []
 
     # URL de la première page de la catégorie de livres
@@ -106,9 +107,7 @@ def categ_to_scrape(base_url, categ):
 
     # Boucle pour parcourir toutes les pages de livres
     while url:
-
         soup = bs_get(url)
-
         # Récupération des informations de chaque livre sur la page
         books = soup.select('article.product_pod')
         for book in books:
@@ -125,7 +124,7 @@ def categ_to_scrape(base_url, categ):
             url = None
 
     load_csv(csv_path, books_datas)
-    print(f'Catégorie {categ} récupérée.')
+    print(f'Catégorie {category_name} récupérée.')
 
 
 def main():
@@ -136,14 +135,14 @@ def main():
     category_links = soup.select('ul.nav-list > li > ul > li > a')
 
     for category_link in category_links:
-        categ = category_link.get_text().strip()
-        categ_slugged = slugify(categ, separator="_")
+        category_name = category_link.get_text().strip()
+        categ_slugged = slugify(category_name, separator="_")
         directory = Path(f'./{categ_slugged}')
         if not directory.exists():
             directory.mkdir(parents=True, exist_ok=True)
 
         url = base_url + category_link['href']
-        categ_to_scrape(url[:-10], categ)
+        get_books_from_category(url[:-10], category_name)
 
 
 if __name__ == "__main__":
